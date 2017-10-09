90f588fc5847d2cfea24f00c9a5daef16fa67b28
Anil Bisalehalli Prasannakumar <anil.kumar@ringcentral.com>
2017-10-09 14:11:19 -0700

    Changes to accomodate bot-provisioner

--- index.php
+++ index.php
@@ -13,11 +13,28 @@ $dotenv->load();
 try {
 
     // Create a Subscription using webhooks method
+//    $rcsdk = new SDK($_ENV['GLIP_APPKEY'], $_ENV['GLIP_APPSECRET'] , $_ENV['GLIP_SERVER'], 'Demo', '1.0.0');
+//
+//    $platform = $rcsdk->platform();
+//
+//    $auth = $platform->login($_ENV['GLIP_USERNAME'], $_ENV['GLIP_EXTENSION'], $_ENV['GLIP_PASSWORD']);
+
+
+    if (!isset($_GET['code'])) {
+        return;
+    }
+
+    // Create SDK instance
     $rcsdk = new SDK($_ENV['GLIP_APPKEY'], $_ENV['GLIP_APPSECRET'] , $_ENV['GLIP_SERVER'], 'Demo', '1.0.0');
 
+    // Create Platform instance
     $platform = $rcsdk->platform();
+    $qs = $platform->parseAuthRedirectUrl($_SERVER['QUERY_STRING']);
+    $qs["redirectUri"] = $_ENV['GLIP_REDIRECT_URL'];
+    $apiResponse = $platform->login($qs);
 
-    $auth = $platform->login($_ENV['GLIP_USERNAME'], $_ENV['GLIP_EXTENSION'], $_ENV['GLIP_PASSWORD']);
+    print 'The Tokens is : ' . PHP_EOL . print_r($apiResponse->text());
+
 
     // Setup the Webhook Subscription
     $apiResponse = $platform->post('/subscription',array(
